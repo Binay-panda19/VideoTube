91cf2d9e4c6089a909508f48d9cbec8472975886
binay <binay885b@gmail.com>
2025-07-13 12:03:48 +0530

    completed user and video models

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,84 @@
 import mongoose , { Schema } from 'mnongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
 
-const userSchema = new Schema({});
+const userSchema = new Schema(
+    {
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase:true,
+        index:true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase:true,
+        trim: true,
+    },
+    fullname:{
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar:{
+        type:String, //cloudinary Url
+        required:true
+    },
+    coverImage:{
+        type:String, //cloudinary Url
+    },
+    watchHistory:[
+        {
+            type: Schema.Types.objectId,
+            ref:"Video"
+        }
+    ],
+    password:{
+        type: String,
+        required: [true,"Password is required"],
+    },
+    refreshToken:{
+        type: String
+    }
+    },
+    {
+        timestamps: true   // Automatically adds createdAt and updatedAt fields
+    }
+);
 
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next();   
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password,this.password)
+}
+userSchema.methods.createJWT = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+            fullname: this.fullname,
+        }, 
+        process.env.JWT_SECRET, 
+        {expiresIn: process.env.JWT_EXPIRATION})
+}
 
-export const user = mongoose.model("User", userSchema);
+userSchema.methods.createRefreshJWT = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: process.env.REFRESH_TOKEN_EXPIRATION}
+    )
+}
+
+export const User = mongoose.model("User", userSchema);
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,6 +1,49 @@
 import mongoose , { Schema } from "mpongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videoSchema = new Schema({});
 
-export const video = mongoose.model("Video" , videoSchemama);
+const videoSchema = new Schema(
+    {
+    videofile:{
+        type:String,  //cloudinary Url
+        required: true
+    },
+    thumbnail:{
+        type: String, //cloudinary Url
+        required: true,
+    },
+    title:{
+        type:String,
+        required: true,
+
+    },
+    description:{
+        type:String,
+        required: true
+    },
+    duration:{
+        type: Number, //cloudinary Url
+        required:true
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+    isPublished:{
+        type:Boolean,
+        default: true
+    },
+    owner:{
+        type:Schema.Types.objectId,
+        ref:"User"
+    }
+},
+{
+    timestamps: true
+}
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video" , videoSchema);
 // This code defines a Mongoose schema for a video model, which can be used to interact with a MongoDB database. The schema is currently empty, meaning it does not define any specific fields or validation rules for the video documents. The model is then exported for use in other parts of the application.
