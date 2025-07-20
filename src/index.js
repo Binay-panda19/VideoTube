5ad7ef0162ab05a627cc52a575df46a3e7ce04d7
binay <binay885b@gmail.com>
2025-07-20 11:11:30 +0530

    refactor: improve code formatting and organization in index.js

--- src/index.js
+++ src/index.js
@@ -1,43 +1,29 @@
 // require ('dotenv').config({path : "./env"})
 import dotenv from "dotenv";
-import connectDB from "./db/index.js"
+import connectDB from "./db/index.js";
 import { app } from "./app.js"; // ✅ Use the configured app
-
 
 // Load environment variables from the .env file
 // Ensure this is done before using any environment variables
 dotenv.config({
-    path : "./env"
-})
-
-
-connectDB()
-.then(() => {
-    console.log("Database connected successfully!");
-   
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
-    });
-    app.get("/",(req,res) =>{
-        res.send("Welcome to the API");
-    })
-})
-.catch((error) => {
-    console.error("Database connection failed:", error);
-    process.exit(1); // Exit the process with failure
+  path: "./env",
 });
 
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully!");
 
-
-
-
-
-
-
-
-
-
-
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+    app.get("/", (req, res) => {
+      res.send("Welcome to the API");
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection failed:", error);
+    process.exit(1); // Exit the process with failure
+  });
 
 /*import express from "express";
 const app = express();
